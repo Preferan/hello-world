d1c9f4ed778fdd3838f7fad7d97a51de3e087ea2
Preferan <49087175+Preferan@users.noreply.github.com>
2021-07-14 18:58:23 +0800

    Create lesson2_2.c

---
+++ lesson2_2.c
@@ -0,0 +1,22 @@
+#include<reg52.h>
+#define uint unsigned int
+#define uchar unsigned char
+sbit D1=P2^0;
+
+void delay();
+
+void main()
+{
+	D1=0;
+	delay();
+	D1=1;
+	delay();
+}
+
+
+void delay()
+{
+	int x,y;
+	for(x=95;x>0;x--)
+		for(y=1200;y>0;y--);
+}
